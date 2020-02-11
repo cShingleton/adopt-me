d420106b21eaceabf290d0f80ed713e522079781
Charlie <charlie.shingleton@wunderdog.fi>
2020-02-11 15:12:59 +0200

    FEAT Retrieve breeds from live API

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import PetAPI, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
@@ -7,11 +7,38 @@ const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pets.animals({
+      location,
+      breed,
+      type: animal
+    });
+    setPets(animals || []);
+  }
+
+  // scheduled to run after render -- have to declare dependencies list
+  // to watch for changes in them, otherwise will fire on every re-render
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    PetAPI.breeds(animal).then(({ breeds: apiBreeds }) => {
+      const breedNames = apiBreeds.map(({ name }) => name);
+      setBreeds(breedNames);
+    }, console.error);
+  }, [animal, setBreeds, setBreed]);
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={ev => {
+          ev.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
