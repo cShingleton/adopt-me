c1638c934ebfb9536e080a8dfc4b4feb263a554b
Charlie <charlie.shingleton@wunderdog.fi>
2020-02-21 14:50:49 +0200

    STYLE Use colors design system

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,16 +1,28 @@
 import React from "react";
 import { Link } from "@reach/router";
 import { css } from "@emotion/core";
+import colors from "./colors";
 
 const NavBar = () => (
   <header
     css={css`
-      background-color: #333;
+      background-color: ${colors.dark};
       padding: 15px;
     `}
   >
     <Link to="/">Adopt Me!</Link>
-    <span aria-label="logo">Woof!</span>
+    <span
+      aria-label="logo"
+      css={css`
+        font-size: 32px;
+        color: ${colors.primary};
+        &:hover {
+          text-decoration: underline;
+        }
+      `}
+    >
+      Woof!
+    </span>
   </header>
 );
 
