e3e419cfc3c21af99029f86fe4ba0d73221c8cc7
Charlie <charlie.shingleton@wunderdog.fi>
2020-02-24 14:59:40 +0200

    REFAC Convert Details to TS

--- src/Details.tsx
+++ src/Details.tsx
@@ -1,21 +1,33 @@
 import React from "react";
-import PetAPI from "@frontendmasters/pet";
-import { navigate } from "@reach/router";
+import PetAPI, { Photo } from "@frontendmasters/pet";
+import { navigate, RouteComponentProps } from "@reach/router";
 
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import Modal from "./Modal";
 import ThemeContext from "./ThemeContext";
 
-class Details extends React.Component {
+class Details extends React.Component<RouteComponentProps<{ id: string }>> {
   // experimental public field declaration
-  state = {
-    loading: false,
-    showModal: false
+  public state = {
+    error: false,
+    loading: true,
+    showModal: false,
+    animal: "",
+    breed: "",
+    location: "",
+    description: "",
+    name: "",
+    media: [] as Photo[],
+    url: ""
   };
 
-  componentDidMount() {
-    this.setState({ loading: true, showModal: false });
+  public componentDidMount() {
+    if (!this.props.id) {
+      navigate("/");
+      return;
+    }
+
     PetAPI.animal(Number(this.props.id))
       .then(({ animal }) => {
         this.setState({
@@ -28,15 +40,16 @@ class Details extends React.Component {
           breed: animal.breeds.primary
         });
       })
-      .catch(e => console.error(e))
+      .catch((e: Error) => this.setState({ error: e }))
       .finally(() => this.setState({ loading: false }));
   }
 
-  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+  public toggleModal = () =>
+    this.setState({ showModal: !this.state.showModal });
 
-  adopt = () => navigate(this.state.url);
+  public adopt = () => navigate(this.state.url);
 
-  render() {
+  public render() {
     if (this.state.loading) {
       return <h1>loading...</h1>;
     }
@@ -59,6 +72,7 @@ class Details extends React.Component {
           <ThemeContext.Consumer>
             {([theme]) => (
               <button
+                type="button"
                 onClick={this.toggleModal}
                 style={{ backgroundColor: theme }}
               >
@@ -72,8 +86,12 @@ class Details extends React.Component {
               <div>
                 <h1>Are you sure you would you like to adopt {name}?</h1>
                 <div className="buttons">
-                  <button onClick={this.adopt}>Yes</button>
-                  <button onClick={this.toggleModal}>No</button>
+                  <button type="button" onClick={this.adopt}>
+                    Yes
+                  </button>
+                  <button type="button" onClick={this.toggleModal}>
+                    No
+                  </button>
                 </div>
               </div>
             </Modal>
@@ -84,7 +102,9 @@ class Details extends React.Component {
   }
 }
 
-export default function DetailsWithErrorBoundary(props) {
+export default function DetailsWithErrorBoundary(
+  props: RouteComponentProps<{ id: string }>
+) {
   return (
     <ErrorBoundary>
       <Details {...props} />
