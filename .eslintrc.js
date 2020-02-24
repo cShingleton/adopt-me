f47f3810990e1d97de728f03c80bc7376a8cc13a
Charlie <charlie.shingleton@wunderdog.fi>
2020-02-24 11:40:36 +0200

    CHORE Configure eslint to handle typescript

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:import/errors",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+    "prettier/react",
+    "prettier/@typescript-eslint"
+  ],
+  rules: {
+    "react/prop-types": 0,
+    "no-console": 1,
+    "react-hooks/rules-of-hooks": 2,
+    "react-hooks/exhaustive-deps": 1
+  },
+  plugins: ["@typescript-eslint", "react", "import", "jsx-a11y", "react-hooks"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    },
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"]
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  }
+};
