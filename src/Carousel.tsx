25ab8cf63413aa48e507fecb82f9791bf406f646
Charlie <charlie.shingleton@wunderdog.fi>
2020-02-24 15:53:00 +0200

    REFAC Convert Carousel to TS

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { Photo } from "@frontendmasters/pet";
 
-class Carousel extends React.Component {
-  state = {
+interface IProps {
+  media: Photo[];
+}
+
+interface IState {
+  active: number;
+  photos: string[];
+}
+
+class Carousel extends React.Component<IProps, IState> {
+  public state = {
     photos: [],
     active: 0
   };
 
-  static getDerivedStateFromProps({ media }) {
+  public static getDerivedStateFromProps({ media }: IProps) {
     let photos = ["http://placecorgi.com/600/600"];
 
     if (media && media.length) {
@@ -15,15 +25,18 @@ class Carousel extends React.Component {
     return { photos };
   }
 
-  handleIndexClick = event => {
-    this.setState({
-      // not going to use unary for type-casting like teacher as I think it is confusing
-      // and Number(...) is both more explicit and provides exact same functionality
-      active: Number(event.target.dataset.index)
-    });
+  public handleIndexClick = (event: React.MouseEvent<HTMLElement>) => {
+    if (!(event.target instanceof HTMLElement)) return;
+    if (event.target.dataset.index) {
+      this.setState({
+        // not going to use unary for type-casting like teacher as I think it is confusing
+        // and Number(...) is both more explicit and provides exact same functionality
+        active: Number(event.target.dataset.index)
+      });
+    }
   };
 
-  render() {
+  public render() {
     const { photos, active } = this.state;
 
     return (
